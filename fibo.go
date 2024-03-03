1677456b85547ddf3ca9dcae14e7e97d5870d6be
Oscar <repuestosperkins@hotmail.com>
2024-03-03 15:58:57 -0300

    Add Fibonacci recursion in Go
    Leonardo of Pisa, commonly known as Fibonacci, was an Italian mathematician
    who lived during the Middle Ages.
    He was born around 1170 AD and died around 1240 AD.

---
+++ fibo.go
@@ -0,0 +1,16 @@
+import "fmt"
+
+func fibonacci(n int) int {
+	if n <= 1 {
+		return n
+	}
+
+	return fibonacci(n-1) + fibonacci(n-2)
+}
+
+func main() {
+	fmt.Println("Fibonacci Sequence")
+	for i := 0; i < 10; i++ {
+		fmt.Printf("%d ", fibonacci(i))
+	}
+}
